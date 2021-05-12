a7c94f3d66ba60cb2525b47fbb6742e990cfe324
MichaelClut <S3897411@student.rmit.edu.au>
2021-05-12 15:43:33 +1000

    Added 4 Hard questions

---
+++ HardQuestionLoops.java
@@ -0,0 +1,36 @@
+public class HardQuestionLoops {
+    public static void main(String args[]) {
+        for (int i = 0; i < 5; i++) {
+            i++;
+            System.out.println(i);
+        }
+        // What is the output?
+    }
+
+    public static void AnswerOpt1() {
+        // 1
+        // 2
+        // 3
+        // 4
+    }
+
+    public static void AnswerOpt2() {
+        // 1
+        // 3
+        // 5
+    }
+
+    public static void AnswerOpt3() {
+        // 0
+        // 1
+        // 2
+        // 3
+        // 4
+    }
+
+    public static void AnswerOpt4() {
+        // 0
+        // 2 
+        // 4
+    }
+}
---
+++ HardQuestionVariable.java
@@ -0,0 +1,25 @@
+public class HardQuestionVariable {
+    public static void main(String args[]) {
+        int x = 1.25;
+        x = x + x;
+        System.out.print(x);
+
+        // What will be the output?
+    }
+
+    public static void AnswerOpt1() {
+        // 2.5
+    }
+
+    public static void AnswerOpt2() {
+        // 2.50
+    }
+
+    public static void AnswerOpt3() {
+        // 2
+    }
+
+    public static void AnswerOpt4() {
+        // Error
+    }
+}
