b0db7e102c7ff36600cdb7d245729dce8d912ccc
Parrthhh <s3897572@student.rmit.edu.au>
2021-05-12 16:53:57 +1000

    Question Folder Changes

---
+++ Questions/MediumQuestionOne.java
@@ -0,0 +1,40 @@
+/**
+ * MediumQuestions
+ */
+public class MediumQuestionOne {
+
+    public static void main(String[] args) {
+
+        // declare myNumber and assign it a value of 10
+        double myNumber = 10.0;
+        /* Step 2: write a variable 'answer' that is the result of raising myNumber to the power of 3, adding 25 to it, 
+        then finding it's total remainder from 12 */
+        double answer = (Math.pow(myNumber, 12.0) + 25) % 12;
+        // Step 3: print 'answer'
+        System.out.println(answer);
+    }
+
+    public static void OptionOne() {
+        double myNumber = 10;
+        int answer = myNumber * myNumber * myNumber + 25 % 3;
+        System.out.println(answer);
+    }
+
+    public static void OptionTwo() {
+        int myNumber = 10;
+        int answer = (Math.pow(myNumber, 12.0) + 25) % 12;
+        System.out.println(answer);
+    }
+
+    public static void OptionThree() {
+        double myNumber = 10.0;
+        double answer = (Math.pow(myNumber, 12.0) + 25) % 12;
+        System.out.println(answer);
+    }
+
+    public static void OptionFour() {
+        double powerNumber = 10.0;
+        double answer = (Math.pow(powerNumber, 12.0) + 25) % 12;
+        System.out.println(answer);
+    }
+}
---
+++ Questions/easyquestionsTwo.java
@@ -0,0 +1,45 @@
+public class easyquestionsTwo {
+    public static void main(String[] args) {
+        
+// Question 2 : Syntax :
+int x;
+int y;
+
+x = 2;
+y = 10;
+
+if( x < 5) 
+System.out.println(x);
+}
+
+else{
+    System.out.println(y)
+}
+
+// Answer : 
+// First one Missing Curly Braces after If Conditon 
+// Second one Missing a semi-colon after System.out.Print Command
+
+public static void OptionOne() {
+    - Incorrect use of whiteSpace in the first If Condtion 
+    - No errors in the Second Condition 
+ 
+ }
+ 
+ public static void OptionTwo() {
+    - Incorrect use of "<" in first If Condtion 
+    - No errors in the second Conditon
+ }
+ 
+ public static void OptionThree() {
+     - First if conditin is missing Curly Braces 
+     - Missing semi-colon in the second- print statment
+ }
+ 
+ public static void OptionFour() {
+    - Incorrect use of the System.out.Print Command
+    - Incorrect use of Logic within the Branches
+ }
+
+    }
+}
