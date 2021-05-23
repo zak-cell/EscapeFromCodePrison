2394a18dcfc0d8c87a8ba78417b46a9d9f42c828
zak-cell <s3812337@student.rmit.edu.au>
2021-05-23 19:08:50 +1000

    widespread edits

--- Questions/Level1Question3.java
+++ Questions/Level1Question3.java
@@ -3,45 +3,33 @@ import java.util.Scanner;
 public class Level1Question3 {
 
    public static void main(String[] args) {
-       
-// Question 3 : ARRAYS
 
-// Will The following Code Output an Even or Odd Number
+    Scanner scnr = new Scanner (System.in);
 
-Scanner scnr = new Scanner (System.in);
+    int[] teamValues = new int[5];
+    int userInput;
+    int i = 0;
 
-int[] teamValues = new int[5];
-int userInput;
-int i = 0;
-
-for( i = 0; i < 3; i++) {
-    userInput = scnr.nextInt();
-    System.out.print(userInput);
-}
-
-// If the User Input is : 1 2 3 4 5 
- // What will be the output after the User's Input?
-
- 
-
-
-
-}
+    for (i = 0; i < 3; i++) {
+        userInput = scnr.nextInt();
+        System.out.print(userInput);
+        }
+    }
 }
 
 
-public void OptionOne(){
-    - 12345
+public void OptionOne() {
+    12345
 }
 
 public void OpitionTwo(){
-    - 1234
+    123
 }
 
 public void OptionThree(){
-    - 123
+    1234
 }
 
 public void OptionFour(){
-    - 54321
+    54321
 }
